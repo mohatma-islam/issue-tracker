b0a1d17da0209280ec2ff6a005faf70bd9443f3c
mohatma <mohatma@fifteen.co.uk>
2023-11-27 14:51:56 +0000

    initial commit

---
+++ app/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { FaBug } from "react-icons/fa";
+import { usePathname } from "next/navigation";
+import classnames from "classnames"; // to install classnames -> npm i classnames@2.3.2
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  console.log(currentPath);
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li>
+            <Link
+              key={link.href}
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
