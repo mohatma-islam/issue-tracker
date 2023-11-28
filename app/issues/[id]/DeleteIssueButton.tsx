631f6cc19f34e7c375690c8e52c8ee9ba5a8ad3a
mohatma <mohatma@fifteen.co.uk>
2023-11-28 17:29:49 +0000

    add delete issue logics

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,10 +1,55 @@
-import { TrashIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+"use client";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import Spinner from "@/app/components/Spinner";
+import { useState } from "react";
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+  const router = useRouter();
 
-const DeleteIssueButton = ({issueId}: {issueId: number}) => {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const handleDelete = async () => {
+    try {
+      setIsDeleting(true);
+      await axios.delete("/api/issues/" + issueId);
+      router.push("/issues");
+      router.refresh();
+    } catch (error) {
+      setIsDeleting(false);
+      console.log("error occured!");
+    }
+  };
   return (
-    <Button color="red"><TrashIcon/> Delete Issue</Button>
-  )
-}
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red" disabled={isDeleting}>
+          <TrashIcon /> Delete Issue
+          {isDeleting && <Spinner />}
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be
+          undone.
+        </AlertDialog.Description>
+        <Flex className="mt-4" gap="3">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red" onClick={handleDelete}>
+              Delete Issue
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
+};
 
-export default DeleteIssueButton
+export default DeleteIssueButton;
