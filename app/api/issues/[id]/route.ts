84982c8a56161a5a9ea864b197dc4866aae944fb
mohatma <mohatma@fifteen.co.uk>
2023-11-29 18:59:50 +0000

    assign and unassign a issue to user

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,61 +1,69 @@
-import { issueSchema } from "@/app/validationSchema";
+import { patchIssueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 import authOptions from "../../auth/authOptions";
 
-export async function  PATCH(
-    request: NextRequest,
-     {params} : {params: {id: string}}){
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+//   const session = await getServerSession(authOptions);
 
-    const session = await getServerSession(authOptions);
+//   if (!session) return NextResponse.json({}, { status: 401 });
 
-    if(!session)
-        return NextResponse.json({}, {status: 401});
-    
-    
-    const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+  const body = await request.json();
+  const validation = patchIssueSchema.safeParse(body);
 
-    if(!validation.success)
-        return NextResponse.json(validation.error.format(), { status: 400});
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
-    const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
-    })
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-    if(!issue)
-        return NextResponse.json({error: 'Invalid Issue Id'}, { status: 400});
+  const { assignedToUserId, title, description } = body;
+  if (assignedToUserId && assignedToUserId !== "null" ) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserId },
+    });
+    if (!user)
+      return NextResponse.json({ error: "Invalid User Id" }, { status: 400 });
+  }
 
-    const updatedIssue = await prisma.issue.update({
-        where: {id: issue.id},
-        data:{
-            title: body.title,
-            description: body.description
-        }
-    })
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue Id" }, { status: 400 });
 
-    return NextResponse.json(updatedIssue);
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title,
+      description,
+      assignedToUserId: assignedToUserId !== "null" ? assignedToUserId : null
+    },
+  });
+
+  return NextResponse.json(updatedIssue);
 }
 
-export async function DELETE( request: NextRequest,
-    {params} : {params: {id: string}}){
-    
-    const session = await getServerSession(authOptions);
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const session = await getServerSession(authOptions);
 
-    if(!session)
-        return NextResponse.json({}, {status: 401});
-    
-    const issue = await prisma.issue.findUnique({
-        where: {id: parseInt(params.id)}
-    })
+  if (!session) return NextResponse.json({}, { status: 401 });
 
-    if(!issue)
-        return NextResponse.json({error: 'Invalid Issue Id'}, { status: 400});
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
 
-    const deleteIssue = await prisma.issue.delete({
-        where: {id: issue.id}
-    })
+  if (!issue)
+    return NextResponse.json({ error: "Invalid Issue Id" }, { status: 400 });
 
-    return NextResponse.json('Issue Deleted!');
+  const deleteIssue = await prisma.issue.delete({
+    where: { id: issue.id },
+  });
+
+  return NextResponse.json("Issue Deleted!");
 }
--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,12 +1,11 @@
 "use client";
-import { User } from "@prisma/client";
+import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
 import { Skeleton } from "@/app/components";
 
-const AssigneeSelect = () => {
+const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
     data: users,
     error,
@@ -14,20 +13,32 @@ const AssigneeSelect = () => {
   } = useQuery<User[]>({
     queryKey: ["users"],
     queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000,  // 60s
-    retry: 3
+    staleTime: 60 * 1000, // 60s
+    retry: 3,
   });
 
-  if(isLoading) return <Skeleton/>
+  // const handleAssginedUser = () =>{
 
-  if(error) return null;
+  // }
+
+  if (isLoading) return <Skeleton />;
+
+  if (error) return null;
 
   return (
-    <Select.Root>
+    <Select.Root
+    defaultValue={issue.assignedToUserId || ""}
+    onValueChange={(userId) => {
+      axios.patch("/api/issues/" + issue.id, {
+        assignedToUserId: userId,
+      });
+    }}
+    >
       <Select.Trigger placeholder="Assign.." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="null">Unassigned</Select.Item>
           {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
